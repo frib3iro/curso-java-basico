42c190057e0a0018a1370b12de2867e651a24cc1
Fabio <Fabio@DESKTOP-3GP7MIM>
2021-04-11 17:27:38 -0300

    Exercicios 12, 13, resolvidos.

--- src/com/fabio/cursojava/aula19/labs/Exer12.java
+++ src/com/fabio/cursojava/aula19/labs/Exer12.java
@@ -1,5 +1,7 @@
 package com.fabio.cursojava.aula19.labs;
 
+import java.util.Scanner;
+
 public class Exer12 {
     
     public static void main(String[] args) {        
@@ -7,5 +9,26 @@ public class Exer12 {
         // Criar um vetor A com 10 elementos inteiros. Implementar um programa
         // que defina e escreva a soma de todos os elementos armazenados
         // neste vetor.
+        Scanner scan = new Scanner(System.in);
+        
+        int[] A = new int[3];
+        int soma = 0;
+        
+        for (int i = 0; i < A.length; i++) {
+            System.out.print("Digite o numero " + i + ": ");
+            A[i] = scan.nextInt();
+            soma = soma + A[i];
+        }
+        
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + " ");
+        }
+        
+        System.out.println();
+        System.out.println("A soma de todos os elementos é: " + soma);
+        
+        
+        scan.close();
     }
 }
--- src/com/fabio/cursojava/aula19/labs/Exer13.java
+++ src/com/fabio/cursojava/aula19/labs/Exer13.java
@@ -1,5 +1,7 @@
 package com.fabio.cursojava.aula19.labs;
 
+import java.util.Scanner;
+
 public class Exer13 {
     
     public static void main(String[] args) {        
@@ -7,5 +9,12 @@ public class Exer13 {
         // Criar um vetor A com 10 elementos inteiros. Implementar um programa
         // que determine a soma dos elementos armazenados neste vetor que
         // são múltiplos de 5.
+        Scanner scan = new Scanner(System.in);
+        
+        int[] A = new int[3];
+        
+        
+        scan.close();
+        
     }
 }

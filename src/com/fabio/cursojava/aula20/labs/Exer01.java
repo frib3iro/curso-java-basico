48c3b2a53306293552b35a64697df8776a602f23
Fabio <Fabio@DESKTOP-3GP7MIM>
2021-04-12 16:39:30 -0300

    Exercicio 01 aula 20

---
+++ src/com/fabio/cursojava/aula20/labs/Exer01.java
@@ -0,0 +1,15 @@
+package com.fabio.cursojava.aula20.labs;
+
+import java.util.Random;
+
+public class Exer01 {
+
+    // Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-9.
+    // Após isso determine o maior número da matriz e a sua posição 
+    // (linha, coluna).
+    Random numRandom = new Random();
+
+    int[][] numAleatorios = new int[4][4];
+
+    
+}

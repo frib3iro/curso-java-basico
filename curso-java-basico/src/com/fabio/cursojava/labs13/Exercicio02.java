448ad2e7bd448da987fae2a8bbe7c33d020400d4
Fabio Ribeiro <rib3iro@live.com>
2024-11-02 10:54:33 -0300

    Exercicio 04 resolvido

--- curso-java-basico/src/com/fabio/cursojava/labs13/Exercicio02.java
+++ curso-java-basico/src/com/fabio/cursojava/labs13/Exercicio02.java
@@ -35,7 +35,6 @@ public class Exercicio02 {
     
 }
 
-
 /*
 Criar um vetor A com 8 elementos inteiros. Construir um vetor B de mesmo tipo e 
 tamanho e com os elementos do vetor A multiplicados por 2, 
--- curso-java-basico/src/com/fabio/cursojava/labs13/Exercicio03.java
+++ curso-java-basico/src/com/fabio/cursojava/labs13/Exercicio03.java
@@ -2,7 +2,7 @@ package com.fabio.cursojava.labs13;
 
 import java.util.Random;
 
-public class Exercicio01 {
+public class Exercicio03 {
     
     public static void main(String[] args) {
 	
@@ -10,32 +10,33 @@ public class Exercicio01 {
 	
 	System.out.println("Hello world!");
 	
-	int[] vetorA = new int[5];
+	int[] vetorA = new int[15];
 	int[] vetorB = new int[vetorA.length];
 	
 	for (int i = 0; i < vetorA.length; i++) {
 	    vetorA[i] = rd.nextInt(10) + 1;
-	    vetorB[i] = vetorA[i];
+	    vetorB[i] = vetorA[i] * vetorA[i];
 	}
 	
-	System.out.print("Vetor A:");
+	System.out.print("Vetor A: ");
 	for (int a : vetorA) {
 	    System.out.printf("%2d ", a);
 	}
 	
 	System.out.println();
 	
-	System.out.print("Vetor B:");
+	System.out.print("Vetor B: ");
 	for (int b : vetorB) {
 	    System.out.printf("%2d ", b);
 	}
-	
+
 	System.out.println();
     }
     
 }
 
 /*
-Criar um vetor A com 5 elementos inteiros. Construir um vetor B de mesmo tipo e 
-tamanho e com os "mesmos" elementos do vetor A, ou seja, B[i] = A[i]. 
+Criar um vetor A com 15 elementos inteiros. Construir um vetor B de mesmo tipo e 
+tamanho, sendo que cada elemento do vetor B deverá ser o quadrado do respectivo 
+elemento de A, ou seja: B[i] = A[i] * A[i].   
 */

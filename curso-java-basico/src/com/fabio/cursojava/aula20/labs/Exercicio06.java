bd53c846befbb570eec87b446330e108bf2bec32
Fabio Ribeiro <rib3iro@live.com>
2024-11-03 17:34:21 -0300

    Inicio do exercício do jogo da velha

--- curso-java-basico/src/com/fabio/cursojava/aula20/labs/Exercicio06.java
+++ curso-java-basico/src/com/fabio/cursojava/aula20/labs/Exercicio06.java
@@ -1,18 +1,42 @@
 package com.fabio.cursojava.aula20.labs;
 
+import java.util.Scanner;
+
 public class Exercicio06 {
 
     public static void main(String[] args) {
 
+	Scanner sc = new Scanner(System.in);
+	
 	System.out.println("Exercício 06");
 
 	int[][] jogoDaVelha = new int[3][3];
 
-	System.out.println();
+	int jogar = 0;
+	
+	while (jogar == 0) {
+	    for (int linha = 0; linha < jogoDaVelha.length; linha++) {
+		System.out.println("-");
+		for (int coluna = 0; coluna < jogoDaVelha[linha].length; coluna++) {
+		    System.out.println("|");
+		}
+		System.out.println();
+	    }
+	    System.out.println("Deseja continuar? ");
+
+	    jogar = sc.nextInt();
+	}
+
+	sc.close();
     }
 }
 
 /*
+
+1 2 3
+4 5 6
+7 8 9
+
 Faça um programa para jogar o jogo da velha. O programa deve permitir que dois 
 jogadores façam uma partida do jogo da velha, usando o computador para ver o 
 tabuleiro. Cada jogador vai alternadamente informando a posição onde deseja 
